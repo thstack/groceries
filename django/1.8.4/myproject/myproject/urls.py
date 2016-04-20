2ac80160f30c1c95ce7b3dce271ed52419cb7934
Simon Luo <lx_9986@hotmail.com>
2016-04-21 06:08:24 +0800

    bugfix

--- django/1.8.4/myproject/myproject/urls.py
+++ django/1.8.4/myproject/myproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from myapp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    # Coding here.
+
 ]
